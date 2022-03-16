90ad9dff35d756933bee04bf1cc0c5f703f093ef
shtafa <ersh0va.varvara@yandex.ru>
2022-03-16 13:30:57 +0300

    first

---
+++ nuxt.config.ts
@@ -0,0 +1,21 @@
+import { defineNuxtConfig } from 'nuxt3'
+
+// https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
+export default defineNuxtConfig({
+    css: ['vuetify/lib/styles/main.sass'],
+    build: {
+        transpile: ['vuetify']
+    },
+    vite: {
+        define: {
+            'process.env.DEBUG': 'false',
+        }
+    },
+    modules: ["@nuxtjs/axios"],
+    axios: {
+        proxyHeaders: false,
+    },
+    buildModules: [
+        '@nuxt/typescript-build',
+    ],
+})
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <section>
+    <v-app>
+      <v-btn>Первая кнопка</v-btn>
+      <v-btn>Вторая кнопка</v-btn>
+      <v-btn>Еще кнопка</v-btn>
+    </v-app>
+  </section>
+</template>
+<script>
+export default {
+
+}
+</script>
