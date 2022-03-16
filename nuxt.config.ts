6608853fb32ba2957ce7a9d9dc8cc00b50fb1251
shtafa <ersh0va.varvara@yandex.ru>
2022-03-16 14:29:13 +0300

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,6 @@ import { defineNuxtConfig } from 'nuxt3'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-    Vite: false,
     css: ['vuetify/lib/styles/main.sass'],
     build: {
         transpile: ['vuetify']
